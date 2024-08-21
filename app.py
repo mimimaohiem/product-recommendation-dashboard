90f58c9245cc477ce529c124e42138d731f1dcbc
Hoainam <roronoadkinatsu@gmail.com>
2024-08-21 19:03:01 +0700

    Initial commit

--- app.py
+++ app.py
@@ -2,54 +2,177 @@ from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 from flask_apscheduler import APScheduler
 from recommendation_module import prepare_recommendation
+import MySQLdb
 import pandas as pd
 
 scheduler = APScheduler()
 app = Flask(__name__, template_folder='site')
 CORS(app)
 
-# Pre-requisites - data connection/loading
-dataset_root = 'static/dataset/'  
-# Loading customer interaction data
-customer_interactions_df = pd.read_csv(f"{dataset_root}generated_customer_interactions.csv")
-customer_interactions_df.columns = ['customer_id', 'customer_page_views', 'customer_time_spent']
-# Loading product details data
-product_details_df = pd.read_csv(f"{dataset_root}generated_product_details.csv", sep=";")
-product_details_df.columns = ['product_id', 'product_category', 'product_price', 'product_ratings']
-# Loading purchasing history data
-purchase_history_df = pd.read_csv(f"{dataset_root}generated_purchase_history.csv", sep=";")
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'recommendation'
+app.config['MYSQL_PASSWORD'] = '123456a'
+app.config['MYSQL_DB'] = 'recommendation_db'
+
+mysql = MySQLdb.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    passwd=app.config['MYSQL_PASSWORD'],
+    db=app.config['MYSQL_DB']
+)
+
+def load_data_from_db():
+    cursor = mysql.cursor()
+
+    # Loading customer interaction data
+    cursor.execute("SELECT customer_id, customer_page_views, customer_time_spent FROM customer_interactions")
+    customer_interactions = cursor.fetchall()
+    customer_interactions_df = pd.DataFrame(customer_interactions, columns=['customer_id', 'customer_page_views', 'customer_time_spent'])
+
+    # Loading product details data
+    cursor.execute("SELECT product_id, product_category, product_price, product_ratings FROM product_details")
+    product_details = cursor.fetchall()
+    product_details_df = pd.DataFrame(product_details, columns=['product_id', 'product_category', 'product_price', 'product_ratings'])
+
+    # Loading purchasing history data
+    cursor.execute("SELECT customer_id, product_id, purchase_date FROM purchase_history")
+    purchase_history = cursor.fetchall()
+    purchase_history_df = pd.DataFrame(purchase_history, columns=['customer_id', 'product_id', 'purchase_date'])
+
+    cursor.close()
+
+    return customer_interactions_df, product_details_df, purchase_history_df
+
+# Load initial data from the database
+customer_interactions_df, product_details_df, purchase_history_df = load_data_from_db()
 
 # Create initial recommendation index for each user
-# Faster latency if we provide the recommendation data first - no need to do calculation every request
 recommendation_index = prepare_recommendation(customer_interactions_df, product_details_df, purchase_history_df, threshold=0.3)
 
-# Scheduler - Update recommendation index every N Hour - in this case, there are no data update (static dataset)
-# Threshold for the similarity between user purchasing behaviour is setted to 0.3
+# Scheduler to update recommendation index every N hours
 @scheduler.task('cron', id='update_dict_job', hour='*/12')
 def scheduled_job():
-    # The result of this recommendation index update will store to global recommendation_index variable
     global recommendation_index
+    customer_interactions_df, product_details_df, purchase_history_df = load_data_from_db()
     recommendation_index = prepare_recommendation(customer_interactions_df, product_details_df, purchase_history_df, threshold=0.3)
 
 @app.route('/')
 def home():
-    # Root route will show the simple dashboard to get the product recommendation of each user based on their purchasing behaviour
-    # To get the recommendation data for each user, in index.html will hit to `/recommendation/<int:customer_id>`
     return render_template('index.html')
 
 @app.route('/recommendation/<int:customer_id>', methods=['GET'])
 def recommendation(customer_id):
     global recommendation_index
+
+    # Get customer information from the users table
+    user_info = get_user_info(customer_id)
+
+    if not user_info:
+        return jsonify({
+            'error': 'User not found'
+        }), 404
+
+    # Fetch recommendation data
     try:
-        # Try to get the recommendation from recommendation index
         response = recommendation_index[customer_id]
-    except:
-        # If failed or no data found in recommendation index, it will return no data like below
+    except KeyError:
         response = {
-            'customer_id' : customer_id,
-            'history' : [],
-            'recommendation' : []
+            'customer_id': customer_id,
+            'history': [],
+            'recommendation': [],
+            'similar_customers': []
         }
+
+    # Combine customer info with recommendation data
+    response['user_info'] = user_info
+
+    # Retrieve similar customers' detailed info
+    similar_customer_ids = [sc['customer_id'] for r in response['recommendation'] for sc in r.get('similar_customers', [])]
+    response['similar_customers'] = get_similar_customers_info(similar_customer_ids)
+
+    return jsonify(response)
+
+def get_user_info(customer_id):
+    cursor = mysql.cursor()
+    query = "SELECT customer_id, name, email, phone FROM users WHERE customer_id = %s"
+    cursor.execute(query, (customer_id,))
+    user_info = cursor.fetchone()
+    cursor.close()
+
+    if user_info:
+        return {
+            'customer_id': user_info[0],
+            'name': user_info[1],
+            'email': user_info[2],
+            'phone': user_info[3]
+        }
+    else:
+        return None
+
+def get_similar_customers_info(customer_ids):
+    if not customer_ids:
+        return []
+
+    cursor = mysql.cursor()
+    format_strings = ','.join(['%s'] * len(customer_ids))
+
+    # Query to get customer info and count of purchases from purchase_history
+    query = f"""
+        SELECT u.customer_id, u.name, COUNT(ph.product_id) as purchased_items
+        FROM users u
+        LEFT JOIN purchase_history ph ON u.customer_id = ph.customer_id
+        WHERE u.customer_id IN ({format_strings})
+        GROUP BY u.customer_id, u.name
+    """
+    cursor.execute(query, tuple(customer_ids))
+    similar_customers = cursor.fetchall()
+    cursor.close()
+
+    return [
+        {
+            'customer_id': customer[0],
+            'name': customer[1],
+            'purchased_items': customer[2]  # The count of purchases
+        } for customer in similar_customers
+    ]
+
+@app.route('/customer-stats/<int:customer_id>', methods=['GET'])
+def customer_stats(customer_id):
+    cursor = mysql.cursor()
+
+    # Fetch customer interactions data (page views and time spent)
+    cursor.execute("""
+        SELECT customer_page_views, customer_time_spent
+        FROM customer_interactions
+        WHERE customer_id = %s
+    """, (customer_id,))
+    interactions = cursor.fetchone()
+
+    if not interactions:
+        return jsonify({"error": "Customer interactions not found"}), 404
+
+    # Fetch number of products purchased from purchase_history
+    cursor.execute("""
+        SELECT COUNT(product_id)
+        FROM purchase_history
+        WHERE customer_id = %s
+    """, (customer_id,))
+    purchases = cursor.fetchone()
+
+    cursor.close()
+
+    if purchases is None:
+        return jsonify({"error": "No purchase history found"}), 404
+
+    # Prepare the response data
+    response = {
+        "customer_id": customer_id,
+        "customer_page_views": interactions[0],
+        "customer_time_spent": interactions[1],
+        "products_purchased": purchases[0]
+    }
+
     return jsonify(response)
 
 if __name__ == '__main__':
--- recommendation_module.py
+++ recommendation_module.py
@@ -1,75 +1,85 @@
 import pandas as pd
 
 def prepare_recommendation(customer_interactions_df, product_details_df, purchase_history_df, threshold=0.3, n_recommendation=20):
-  # Extend information for purchasing history data with customer_interactions data and product_details data
-  dataset = pd.merge(purchase_history_df, customer_interactions_df, on='customer_id', how='left')
-  dataset = pd.merge(dataset, product_details_df, on='product_id', how='left')
-  # Select feature that important to the pipeline used
-  dataset = dataset[['customer_id', 'product_id', 'product_ratings']]
+    # Extend information for purchasing history data with customer_interactions data and product_details data
+    dataset = pd.merge(purchase_history_df, customer_interactions_df, on='customer_id', how='left')
+    dataset = pd.merge(dataset, product_details_df, on='product_id', how='left')
+    # Select feature that is important to the pipeline used
+    dataset = dataset[['customer_id', 'product_id', 'product_ratings']]
 
-  # Craete pivot table to get summary of product rating by each user
-  ratings_matrix_fillna = pd.pivot_table(dataset, index='customer_id', columns='product_id', values='product_ratings')
-  # Normalize the value of the ratings - mean-centered overlapping-item - better result - sum all without NaN - Substract the initial value with it
-  # Do nothing to NaN value - user not rated the product yet or never seen it before
-  ratings_matrix_fillna_norm = ratings_matrix_fillna.subtract(ratings_matrix_fillna.mean(axis=1), axis=0)
-  # Tranpose the matrix to switch the rows and columns to create correlation data between user - .corr() are comparing columns
-  # Get the correaltion for each user to other user based on their purchasing history
-  customer_similarity_matrix = ratings_matrix_fillna_norm.T.corr()
+    # Create pivot table to get a summary of product ratings by each user
+    ratings_matrix_fillna = pd.pivot_table(dataset, index='customer_id', columns='product_id', values='product_ratings')
 
-  # list all unique customer_id
-  customer_list = list(set(customer_interactions_df['customer_id']))
-  # Variable to store recommendation index for every user
-  customers_recommendation_lists = {}
-  for customer_id in customer_list:
-    # Drop row with initial customer_id - customer_id that will be created the recommendation
-    customer_similarity_matrix_comparison = customer_similarity_matrix.copy()
-    customer_similarity_matrix_comparison.drop(index=customer_id, inplace=True)
-    # Find the similar to the user - having high correlated/similar purchasing history vector above threshold
-    similar_customer = customer_similarity_matrix_comparison[customer_similarity_matrix_comparison[customer_id]>threshold][customer_id]
+    # Normalize the value of the ratings - mean-centered overlapping-item - better result - sum all without NaN - Subtract the initial value with it
+    ratings_matrix_fillna_norm = ratings_matrix_fillna.subtract(ratings_matrix_fillna.mean(axis=1), axis=0)
 
-    # Get purchasing history for initial customer_id
-    customer_id_purchased = ratings_matrix_fillna_norm[ratings_matrix_fillna_norm.index == customer_id].dropna(axis=1, how='all')
-    # Get purchasing history for similar customer to initial customer_id, drop product/column with all user never purchased
-    similar_customer_purchased = ratings_matrix_fillna_norm[ratings_matrix_fillna_norm.index.isin(similar_customer.index)].dropna(axis=1, how='all')
-    # Drop product/columns that have been purchased by initial customer
-    similar_customer_purchased.drop(customer_id_purchased.columns, axis=1, inplace=True, errors='ignore')
+    # Transpose the matrix to switch the rows and columns to create correlation data between users
+    customer_similarity_matrix = ratings_matrix_fillna_norm.T.corr()
 
-    # Get product_id and sort by the mean of similarity score from each user, dont include the NaN value
-    recommendation_list = [(product, similar_customer_purchased[product].dropna(axis=0).mean()) for product in similar_customer_purchased]
-    recommendation_list = sorted(recommendation_list, key=lambda x: x[1], reverse=True)
+    # List all unique customer_id
+    customer_list = list(set(customer_interactions_df['customer_id']))
+    # Variable to store recommendation index for every user
+    customers_recommendation_lists = {}
+    for customer_id in customer_list:
+        # Drop row with initial customer_id - customer_id that will be created the recommendation
+        customer_similarity_matrix_comparison = customer_similarity_matrix.copy()
+        customer_similarity_matrix_comparison.drop(index=customer_id, inplace=True)
+        
+        # Find similar users - having highly correlated/similar purchasing history vector above threshold
+        similar_customer = customer_similarity_matrix_comparison[customer_similarity_matrix_comparison[customer_id] > threshold][customer_id]
 
-    # Prepare the purchasing history for initial customer_id
-    history = []
-    for product in list(customer_id_purchased.columns):
-      details = product_details_df[product_details_df['product_id'] == product]
-      product_details = {
-          'product_id' : details.values[0][0],
-          'product_category' : details.values[0][1],
-          'product_price' : details.values[0][2],
-          'product_rating' : details.values[0][3],
+        # Get purchasing history for the initial customer_id
+        customer_id_purchased = ratings_matrix_fillna_norm[ratings_matrix_fillna_norm.index == customer_id].dropna(axis=1, how='all')
+        # Get purchasing history for similar customers, drop products/columns where no user has purchased
+        similar_customer_purchased = ratings_matrix_fillna_norm[ratings_matrix_fillna_norm.index.isin(similar_customer.index)].dropna(axis=1, how='all')
+        # Drop products that have already been purchased by the initial customer
+        similar_customer_purchased.drop(customer_id_purchased.columns, axis=1, inplace=True, errors='ignore')
 
-      }
-      history.append(product_details)
-    
-    # Prepare the recommendation data for initial customer_id
-    recommendation = []
-    for product in recommendation_list[:n_recommendation]:
-      details = product_details_df[product_details_df['product_id'] == product[0]]
-      product_details = {
-          'product_id' : details.values[0][0],
-          'product_category' : details.values[0][1],
-          'product_price' : details.values[0][2],
-          'product_rating' : details.values[0][3],
-      }
-      recommendation.append(product_details)
+        # Get product_id and sort by the mean of similarity score from each user, don't include NaN values
+        recommendation_list = [(product, similar_customer_purchased[product].dropna(axis=0).mean()) for product in similar_customer_purchased]
+        recommendation_list = sorted(recommendation_list, key=lambda x: x[1], reverse=True)
 
-    # Store to form dictionary
-    recommendation_for_a_customer = {
-      'customer_id' : customer_id,
-      'history' : history,
-      'recommendation' : recommendation
-    }
-    # Store recommendation data for each customer to dictionary/database - recommendation index
-    customers_recommendation_lists[customer_id] = recommendation_for_a_customer
-  
-  return customers_recommendation_lists
+        # Prepare the purchasing history for the initial customer_id
+        history = []
+        for product in list(customer_id_purchased.columns):
+            details = product_details_df[product_details_df['product_id'] == product]
+            product_details = {
+                'product_id': int(details.values[0][0]),  # Convert to int
+                'product_category': details.values[0][1],
+                'product_price': float(details.values[0][2]),  # Convert to float
+                'product_rating': float(details.values[0][3]),  # Convert to float
+            }
+            history.append(product_details)
+
+        # Prepare the recommendation data for the initial customer_id
+        recommendation = []
+        for product in recommendation_list[:n_recommendation]:
+            details = product_details_df[product_details_df['product_id'] == product[0]]
+
+            # Get the list of similar customers and their similarity scores for this product
+            similar_customers_for_product = [
+                {"customer_id": int(similar_customer_id), "similarity_score": round(float(similarity_score), 2)}
+                for similar_customer_id, similarity_score in similar_customer.items()
+                if product[0] in ratings_matrix_fillna.loc[similar_customer_id].dropna().index
+            ]
+
+            product_details = {
+                'product_id': int(details.values[0][0]),  # Convert to int
+                'product_category': details.values[0][1],
+                'product_price': float(details.values[0][2]),  # Convert to float
+                'product_rating': float(details.values[0][3]),  # Convert to float
+                'score': round(product[1], 2),  # Add the score here
+                'similar_customers': similar_customers_for_product
+            }
+            recommendation.append(product_details)
+
+        # Store the recommendation data for each customer to the dictionary/database - recommendation index
+        recommendation_for_a_customer = {
+            'customer_id': int(customer_id),  # Convert to int
+            'history': history,
+            'recommendation': recommendation
+        }
+
+        customers_recommendation_lists[customer_id] = recommendation_for_a_customer
+
+    return customers_recommendation_lists
